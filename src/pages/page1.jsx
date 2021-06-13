72ecf5bc6004d0ed3f2ee0b2b73bdf6c4bd086e5
krsntn <kanahan0110@gmail.com>
2021-06-13 21:56:58 +0800

    add practices
    - fetch
    - flatten data
    - sorting
    - searching

--- src/pages/page1.jsx
+++ src/pages/page1.jsx
@@ -1,27 +1,87 @@
-import { Fragment } from 'react';
-import _ from 'lodash';
-import styles from 'Styles/index.module.scss';
+import { useState, useEffect } from 'react';
+// import styles from 'Styles/index.module.scss';
 
-const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function flattenObj(obj) {
+  let newObj = {};
+
+  if (obj) {
+    Object.keys(obj).forEach((key) => {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        newObj = { ...newObj, ...flattenObj(obj[key]) };
+      } else {
+        newObj[key] = obj[key] || null;
+      }
+    });
+  }
+
+  return newObj;
+}
 
 const Page1 = () => {
+  const [users, setUsers] = useState(null);
+  const [searchValue, setSearchValue] = useState(null);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=20')
+      .then((res) => res.json())
+      .then((data) => setUsers(data.results.map((item) => flattenObj(item))));
+  }, []);
+
+  function sorting(sortKey, list) {
+    if (sortKey) {
+      const newUsers = list.sort((a, b) => {
+        if (a[sortKey] < b[sortKey]) return -1;
+        if (a[sortKey] > b[sortKey]) return 1;
+        return 0;
+      });
+      setUsers(newUsers);
+    }
+  }
+
+  function getFilterList(list) {
+    if (searchValue) {
+      return list.filter((row) =>
+        JSON.stringify(row).toLowerCase().includes(searchValue),
+      );
+    }
+    return list;
+  }
+
   return (
-    <Fragment>
-      <div className={styles.container}>
-        <div className={styles.title}>Page 1</div>
-      </div>
+    <div className="App">
+      {/* {users && 
+        <pre>{JSON.stringify(users, undefined, 2)}</pre>
+      } */}
       <div>
-        {_.map(arr, (item) => (
-          <div key={item}>{item}</div>
-        ))}
-        {_.map(
-          _.filter(arr, (item) => item < 5),
-          (num) => (
-            <div key={num}>{num}</div>
-          ),
-        )}
+        <input type="text" onChange={(e) => setSearchValue(e.target.value)} />
       </div>
-    </Fragment>
+      {users && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(users[0]).map((key) => (
+                <th
+                  key={key}
+                  onClick={() =>
+                    sorting(key, JSON.parse(JSON.stringify(users)))
+                  }>
+                  {key}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {getFilterList(users).map((user, index) => (
+              <tr key={index}>
+                {Object.keys(user).map((key) => (
+                  <td key={key}>{user[key]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
